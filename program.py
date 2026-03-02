bf5bc8f10693934c108dd39ee1f0ce265aeacfdf
GabrielVerdin <gabrielg.verdin@gmail.com>
2026-03-02 14:38:05 -0300

    atualização na lógica

--- program.py
+++ program.py
@@ -1,17 +1,19 @@
 print("======= BMI Calculator =======")
 
 name = str(input("\nEnter your Name: "))
-measure = float(input("Enter your measure: "))
-height = float(input("Enter your height: "))
+height = float(input("Enter your height (m): "))
+weight = float(input("Enter your weight (kg): "))
 
-(bmi) = height / (measure * measure)
+bmi = weight / (height * height)
 
 print(f"\nYour BMI is: {bmi:.2f}")
 
 print("\n------- Your Status -------")
-if(bmi >= 18.5 and bmi <= 24,9):
-    print("Normal Height")
-elif (bmi > 24.9 and bmi <= 29.9):
+if bmi >= 18.5 and bmi <= 24.9:
+    print("Normal Weight")
+elif bmi > 24.9 and bmi <= 29.9:
     print("Overweight")
-else:
+elif bmi > 29.9:
     print("Obese")
+else:
+    print("Underweight")
